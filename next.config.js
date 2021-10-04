d5bc02676e0d8e041e5ddfa193c41797c650b55c
cosmas <ezejioforcosmas@gmail.com>
2021-10-04 10:00:32 +0100

    image sizes in config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['images.pexels.com'],
-      deviceSizes: [149, 600, 640, 750, 828, 1080, 1200, 1290, 1920, 2048, 3840],
+      deviceSizes: [130, 149, 282, 640, 750, 828, 1080, 1200, 1290, 1920, 2048, 3840],
   },
 }
